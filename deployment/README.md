00828009b9726ef20a47129b2add0c491c1e8eed
joshua-mjjj <joshuajosh423@gmail.com>
2022-09-23 13:30:29 +0300

    Initial commit

--- deployment/README.md
+++
@@ -1,2 +0,0 @@
-# VIPPU UI
-
--- deployment/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders sign in link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Sign In/i);
-  expect(linkElement).toBeInTheDocument();
-});
